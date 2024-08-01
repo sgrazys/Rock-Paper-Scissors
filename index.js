a3f66dee86e985b6c226bd2298817a8d8db768a3
Saulius Grazys <s.grazys@gmail.com>
2024-08-02 00:16:43 +0300

    add human choice funtion

--- index.js
+++ index.js
@@ -13,4 +13,27 @@ function getComputedChoice() {
 	return computerChoice;
 }
 
-console.log(getComputedChoice());
+console.log('Computer choice is: ', getComputedChoice());
+
+function getHumanChoice() {
+	// get human choice and store in variable
+	const humanChoice = prompt('Please enter your choice: rock, paper or scissors');
+
+	// check IF there is user input. If no return err msg, ELSE format input to lowercase
+	let formattedHumanChoice;
+	if (humanChoice === '' || humanChoice === null) {
+		return 'ERROR: Please enter choice: rock, paper or scissors';
+	} else {
+		formattedHumanChoice = humanChoice.toLocaleLowerCase();
+	}
+
+	// check IF entered data is valid, otherwise return err message
+	if (formattedHumanChoice !== 'rock' && formattedHumanChoice !== 'paper' && formattedHumanChoice !== 'scissors') {
+		return 'ERROR: Please enter only valid choice: rock, paper or scissors';
+	}
+
+	// return formatted human choice
+	return formattedHumanChoice;
+}
+
+console.log('Human choice is: ', getHumanChoice());
