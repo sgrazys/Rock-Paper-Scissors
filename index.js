8a660acc2f9f9b73b8c7355e06ba7d87e9c26f30
Saulius Grazys <s.grazys@gmail.com>
2024-08-01 23:27:14 +0300

    add function returns computer choice

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('HELLO');
+console.log('Welcome to RPS game!');
+
+function getComputedChoice() {
+	// initiate var includes data ['rock', 'paper', 'scissors']
+	const rpsChoices = ['rock', 'paper', 'scissors'];
+
+	// create random number representing index  data. max num is items' num in array
+	const numChoices = rpsChoices.length;
+	const randomIndex = Math.floor(Math.random() * numChoices);
+
+	// return computer choice
+	const computerChoice = rpsChoices[randomIndex];
+	return computerChoice;
+}
+
+console.log(getComputedChoice());
